1a864ee91869852983c273ee4474f3aaee39b1a9
Theo Yochum <theoyochum@gmail.com>
2019-06-15 15:53:39 -0400

    Add post model

--- backend/website/models.py
+++ backend/website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    body = models.TextField()
+
+    def _str_(self):
+        return self.title
