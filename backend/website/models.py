3c165e0d333def6ff279dcc53033595e24db39e1
Theo Yochum <theoyochum@gmail.com>
2019-06-15 15:55:46 -0400

    Add published property to post model

--- backend/website/models.py
+++ backend/website/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     body = models.TextField()
+    published = models.BooleanField(default=False)
 
     def _str_(self):
         return self.title
